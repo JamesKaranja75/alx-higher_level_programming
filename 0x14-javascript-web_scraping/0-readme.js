052c9bf847572ff3974d6b9ab5e037f6379b950e
JamesKaranja75 <karanjajames101@gmail.com>
2024-04-23 02:02:51 -0700

    updates

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -10,8 +10,8 @@ fs.readFile(process.argv[2], 'utf8', function (error, content) {
   if (error) {
     // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
     console.error('Error reading the file:', error);
-
-  } else {
+  } 
+  else {
     // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
     console.log(content);
   }
